1a79db31bbddffc88e9a6eea206940d3e18cb6ec
matheusscarvalho70-web <matheusscarvalho70@gmail.com>
2026-08-12 15:54:14 -0300

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,17 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { Toaster } from "@/components/ui/sonner";
+import React from "react";
 
 export const Route = createRootRoute({
   component: RootComponent,
+  errorComponent: ({ error }) => {
+    return (
+      <div style={{ padding: 20, fontFamily: "sans-serif", color: "red" }}>
+        <h2>Ops! Ocorreu um erro ao carregar o app:</h2>
+        <pre>{error.message}</pre>
+      </div>
+    );
+  },
 });
 
 function RootComponent() {
