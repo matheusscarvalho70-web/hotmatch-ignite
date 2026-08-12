2682f5ecbf8061eed54ebdd8a252bcd8541c5843
matheusscarvalho70-web <matheusscarvalho70@gmail.com>
2026-08-12 16:01:33 -0300

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,9 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import {
+  Outlet,
+  createRootRoute,
+  HeadContent,
+  Scripts,
+} from "@tanstack/react-router";
 import { Toaster } from "@/components/ui/sonner";
 
 export const Route = createRootRoute({
@@ -7,15 +12,17 @@ export const Route = createRootRoute({
 
 function RootComponent() {
   return (
-    <html>
+    <html lang="pt-BR">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>HotMatch</title>
+        <HeadContent />
       </head>
       <body>
         <Outlet />
         <Toaster position="top-center" richColors />
+        <Scripts />
       </body>
     </html>
   );
