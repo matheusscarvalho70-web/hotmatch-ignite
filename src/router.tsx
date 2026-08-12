d0052356811ad00629844314bb9511e007d2b1e0
matheusscarvalho70-web <matheusscarvalho70@gmail.com>
2026-08-12 14:07:27 -0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,11 @@
-import { createRouter as createTanStackRouter } from "@tanstack/react-router";
+import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { QueryClient } from "@tanstack/react-query";
 
-export function createRouter() {
+export function getRouter() {
   const queryClient = new QueryClient();
 
-  const router = createTanStackRouter({
+  const router = createRouter({
     routeTree,
     context: { queryClient },
     defaultPreload: "intent",
@@ -16,6 +16,6 @@ export function createRouter() {
 
 declare module "@tanstack/react-router" {
   interface Register {
-    router: ReturnType<typeof createRouter>;
+    router: ReturnType<typeof getRouter>;
   }
 }
