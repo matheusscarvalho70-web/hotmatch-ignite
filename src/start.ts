08fe0d00c43939e1bc8b820c19282cb9a0ad0b88
matheusscarvalho70-web <matheusscarvalho70@gmail.com>
2026-08-12 21:45:47 -0300

    Update start.ts

--- src/start.ts
+++ src/start.ts
@@ -1,6 +1,3 @@
 import { createStart } from '@tanstack/react-start';
 
-export const startInstance = createStart({
-  // Se o seu middleware estiver causando conflito de tipos, 
-  // tente temporariamente removê-lo para testar o build na Vercel:
-});
+export const startInstance = createStart({});
