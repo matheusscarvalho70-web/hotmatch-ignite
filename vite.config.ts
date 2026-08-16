84a923bf1131a199dffbb8e3bcee230853f713d0
matheusscarvalho70-web <matheusscarvalho70@gmail.com>
2026-08-16 18:53:07 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
-import { nitro } from 'nitro/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     tanstackStart(),
-    nitro({
-      preset: 'vercel',
-    }),
     react(),
   ],
-  ssr: {
-    noExternal: true,
-  },
 });
