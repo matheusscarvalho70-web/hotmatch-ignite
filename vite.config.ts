f9e4794297d9901a7570e3454a9b66206a99665d
matheusscarvalho70-web <matheusscarvalho70@gmail.com>
2026-08-17 15:25:52 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { tanstackStart } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    tanstackStart(),
+    tsconfigPaths(),
+  ],
 })
