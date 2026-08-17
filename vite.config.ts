df7022a1a2cc9c3f40fb8853164d7552ca7040ef
matheusscarvalho70-web <matheusscarvalho70@gmail.com>
2026-08-17 17:03:24 -0300

    Add React plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
     tsconfigPaths(),
+    react(),
   ],
 })
